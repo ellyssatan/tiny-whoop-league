540214c39e7fcd698ce43ff5a16fa803e777d25a
curlynn <curlynntan@yahoo.com.sg>
2022-12-02 14:05:43 +0800

    index.html added

--- src/main/java/vttp_paf/drone/controllers/RaceController.java
+++ src/main/java/vttp_paf/drone/controllers/RaceController.java
@@ -1,5 +1,17 @@
 package vttp_paf.drone.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping
 public class RaceController {
+
+    @RequestMapping("/raceList")
+    public String raceListPage() {
+        return "raceList";
+    }
+
+
     
 }
